26b35440ffd04451d839f02035964a40de269d17
Faareha Raza <164369218+Faareha59@users.noreply.github.com>
2025-11-28 02:21:39 +0500

    Change header from 'My Notes Apps' to 'My Todo Apps'

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1> My Notes Apps..</h1>
+      <h1> My Todo Apps....</h1>
       <div className="input-area">
         <textarea
           placeholder="Write a note.."
